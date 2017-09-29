aae843ac321cec93a76d939073474a20a4fc7e61
Pushkar Sawant <pushkar@engagio.com>
2017-09-29 15:58:14 -0700

    Added referrer and certificate info.

--- elb2loggly.js
+++ elb2loggly.js
@@ -66,7 +66,8 @@ var COLUMNS = [
   'ssl_protocol', // 19
   'target_group_arn', // 20
   'trace_id', // 21
-  'empty' //22
+  'referrer', //22
+  'cert'
 ];
 
 // The following column indexes will be turned into numbers so that
@@ -119,12 +120,13 @@ var obscureURLParameter = function(url, parameter, obscureLength) {
 // Parse elb log into component parts.
 var parseS3Log = function(data, encoding, done) {
   var originalData = data;
-
+  //console.error(data)
+  //console.error(COLUMNS)
   // The trace_id field is optional
   if (data.length === 1) {
     done();
   }
-  else if (data.length === 17 || data.length === 18 || data.length === 19) {
+  else if (data.length === 17 || data.length === 18 || data.length === 19 || data.length === 20) {
       // Split clientip:port and backendip:port at index 3,4
       // We need to be careful here because of potential 5xx errors which may not include
       // backend:port
